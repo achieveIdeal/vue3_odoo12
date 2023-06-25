6c4ee70637f4b6ada15262ff2c2703215d6eba8a
易泰舵 <taiduo.yi@jgdt.com>
2023-06-25 18:54:35 +0800

    格式调整

--- base_front_view/static/src/js/MinPack.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./MinPack.vue_vue_type_script_setup_true_lang.js";import"./RecordView.js";import"./index.js";import"./el-overlay.js";export{e as default};
--- src/views/code_manager/MinPack.vue
+++ src/views/code_manager/MinPack.vue
@@ -33,7 +33,6 @@ const params = reactive({
   domain: [['supplier_id', '=', supplier_id]],
   sort: 'id desc',
   model: 'srm.coding',
-  groupby: 'default_code',
   fields: [
     'name',
     'supplier_id',
@@ -117,7 +116,6 @@ const extras = {
       width: '200'
     }
   },
-  groupby: ['default_code'],
   invisible: ['is_generate', 'state', 'delivery_order_id'],
   listInvisible: ['supplier_id', 'shelf_life', 'manufacturer_id',
     'produce_number', 'comment',],
